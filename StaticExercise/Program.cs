aca9ff812bee08b2f2bd986001c5ba52b31a33f6
Scott <ScottAdelman97@gmail.com>
2022-07-15 13:11:40 -0600

    Temperature Converter

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a temperature in Fahrenheit to convert to Celsius");
+            var celsius = TempConverter.FahrenheitToCelsius();
+            Console.WriteLine(celsius);
+            
+            Console.WriteLine("Give me a temperature in Celsius to conver to Fahrenheit");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit();
+            Console.WriteLine(fahrenheit);
+        }
+
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius()
+        {
+            var fahrenheit = double.Parse(Console.ReadLine());
+            var toCelsius = (fahrenheit - 32) / 1.8;
+            return toCelsius;
+        }
+        public static double CelsiusToFahrenheit()
+        {
+            var celsius = double.Parse(Console.ReadLine());
+            var toFahrenheit = (celsius * 9) / 5 + 32;
+            return toFahrenheit;
+        }
+    }
+}
